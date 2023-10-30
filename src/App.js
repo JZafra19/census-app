7e5cfb62360b904a0f87dd89d8172f67eee8d5ca
JOHN-ZAFRA\johnz <johnzafrawork@gmail.com>
2023-10-30 15:35:19 -0400

    Census app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Census from './component/Census';
+import Capstone from './component/Capstone';
 
 function App() {
   return (
@@ -7,7 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Census></Census>
+          <Capstone></Capstone>
         </p>
         <a
           className="App-link"
